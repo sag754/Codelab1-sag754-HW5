7892199fc0ab36926bed7dea4b712d9312fbfd87
sag754 <56234238+sag754@users.noreply.github.com>
2020-03-12 12:37:20 -0400

    Added player controller, coin, and game manager scripts and animations.

---
+++ CodeLab1-sag754-HW5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float Score = 0;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    public AudioSource source;
+
+    public static PlayerController instance;
+
+    Vector2 movement;
+
+    private void Awake()
+    {
+        if (instance == null) //instance hasn't been set yet
+        {
+            instance = this; //set instance to this object
+            DontDestroyOnLoad(gameObject); //Dont Destroy this object when you load a new scene
+        }
+        else
+        { //if the instance is already set to an object
+            Destroy(gameObject); //destroy this new object, so there is only ever one
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Verticle", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Score++;
+            Debug.Log("Score: " + Score);
+            source.Play();
+        }
+    }
+}
