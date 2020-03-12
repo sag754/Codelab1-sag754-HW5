ce8158b3368f8171c82be221e23dac84a56a7f2b
sag754 <56234238+sag754@users.noreply.github.com>
2020-03-12 12:37:59 -0400

    Added player controller, coin, and game manager.  As well as sounds.

---
+++ CodeLab1-sag754-HW5/Assets/Scripts/Coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //If another GameObject with a 2D Collider on it hits this GameObject's collider
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ CodeLab1-sag754-HW5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Text infoText;
+    public Text infoTime;
+
+    public float timer = 0; //start timer at 0 and keeps track of time
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        infoText.text = "Coins x " + PlayerController.instance.Score;
+        infoTime.text = "Time: " + (int)timer;
+    }
+}
