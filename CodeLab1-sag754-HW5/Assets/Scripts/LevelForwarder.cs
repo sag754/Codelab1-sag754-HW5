f61d311dc1f8a5ea9a4b4d547af3bbd6044972dc
sag754 <56234238+sag754@users.noreply.github.com>
2020-03-23 13:13:09 -0400

    Fixed level forwarder.

--- CodeLab1-sag754-HW5/Assets/Scripts/LevelForwarder.cs
+++ CodeLab1-sag754-HW5/Assets/Scripts/LevelForwarder.cs
@@ -22,7 +22,7 @@ public class LevelForwarder : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         currentLevel++;
         SceneManager.LoadScene(currentLevel);
